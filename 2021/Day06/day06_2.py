b4d9d953e85c2e7fedb62251be701d662f79f270
João Fernandes <93550236+joaocpfernandess@users.noreply.github.com>
2022-06-16 14:08:25 +0200

    Add files via upload

--- 2021/Day06/day06_2.py
+++ 2021/Day06/day06_2.py
@@ -1,50 +1,34 @@
 file = open('input.txt', 'r')
-"""
-class Lanternfish:
 
-    def __init__(self, days):
-        self.days = days
-
-    def sonsindays(self, d):
-        sons = 0
-        for j in range(d):
-            self.days = self.days - 1 if self.days > 0 else 6
-            if self.days == 6:
-                sons += 1
-                b = Lanternfish(8)
-                sons += b.sonsindays(d-j)
-        return sons
-
-
-
-res = 0
-for l in init:
-    fish = Lanternfish(l)
-    res += fish.sonsindays(256)
-print(res)
-
-def sons(n, days):
-    first_son = days - n - 1
-    days_of_birth = [first_son-7*i for i in range(int(first_son/7)+1)]
-    if first_son > 0:
-        return int((days-n-1)/7)+1+sum([sons(8, d) for d in days_of_birth])
+init = [int(n) for n in file.readline().split(',')]
+start_dict = {}
+for k in init:
+    if k in start_dict:
+        start_dict[k] += 1
     else:
-        return 0
+        start_dict[k] = 1
 
-init = [int(n) for n in file.readline().split(',')]
-res, nfish = 0, 0
-for n in init:
-    res += sons(n, 256)
-    print(f"FISH {nfish}: {res}")
-print(res)
-"""
 
-init = [int(n) for n in file.readline().split(',')]
-print(init)
-for k in range(256):
-    print(k)
-    add = [8 for i in init if i == 0]
-    init = list(map(lambda x: x-1 if x > 0 else 6, init))
-    init += add
-print(len(init))
+def laternfish_simulation(occurrences, n_steps):
+    for _ in range(n_steps):
+        new_occ = {}
+        for k in occurrences:
+            if k == 0:
+                new_occ[8] = occurrences[k]
+                if 6 in new_occ:
+                    new_occ[6] += occurrences[k]
+                else:
+                    new_occ[6] = occurrences[k]
+            else:
+                if k-1 == 6 and 6 in new_occ:
+                    new_occ[k-1] += occurrences[k]
+                else:
+                    new_occ[k-1] = occurrences[k]
+        occurrences = new_occ
+    return occurrences
+
+
+counts = laternfish_simulation(start_dict, 256)
+print(f'FINAL ANSWER: {sum(counts.values())}')
+
 
